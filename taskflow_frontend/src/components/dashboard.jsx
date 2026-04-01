44950743bb224a7989b0a92a80d22f76ce6f63df
shivansh983 <121644927+shivansh983@users.noreply.github.com>
2026-04-01 07:41:09 +0530

    Update dashboard.jsx

--- taskflow_frontend/src/components/dashboard.jsx
+++ taskflow_frontend/src/components/dashboard.jsx
@@ -4,7 +4,7 @@ import ProjectBoard from './ProjectBoard';
 import InviteModal from './invitemodel';
 import NotificationBell from './NotificationBell';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 function Dashboard({ onLogout, globalUserRole }) {
   console.log('Dashboard rendered with globalUserRole:', globalUserRole);
