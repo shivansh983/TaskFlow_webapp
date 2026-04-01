bb789c534c7d7c64575fd78c04fe1866fe68a138
shivansh983 <121644927+shivansh983@users.noreply.github.com>
2026-04-01 06:22:56 +0530

    Update settings.py
    changed middleware as well as removingg hardcoded and making env

--- taskflow_backend/taskflow_backend/settings.py
+++ taskflow_backend/taskflow_backend/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Add this at top
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
